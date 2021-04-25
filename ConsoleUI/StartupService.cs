b6963c9986708de003ece91d4dadcac845dd5922
Artem Tokarevskikh <tokarevskikh@gmail.com>
2021-04-25 22:01:45 +0300

    The first example with delay.

--- ConsoleUI/StartupService.cs
+++ ConsoleUI/StartupService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+using System;
+using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
@@ -17,10 +18,18 @@ namespace ConsoleUI
 
         public void Run()
         {
-            for (int i = 0; i < config.GetValue<int>("LoopTimes"); i++)
-            {
-                logger.LogInformation("Run numbber {runNumber}", i);
-            }
+            TestDelay();
+            Console.Read();
+        }
+
+        async void TestDelay()
+        {
+            logger.LogInformation("Start testing delay result");
+            var delay = new chapter_01.Delay();
+            int delayInSeconds = 3;
+            var result = await delay.DelayResult(42, TimeSpan.FromSeconds(delayInSeconds));
+            logger.LogInformation("After delay {delayInSeconds} seconds returned {result}.", delayInSeconds, result);
+            logger.LogInformation("End testing delay result");
         }
     }
 }
